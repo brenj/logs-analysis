1461b874c992154afb9e3c084c0764ca7f5944ba
brenj <brenj@mailbox.org>
2018-01-05 09:52:33 -0600

    Move SQL queries to separate module

---
+++ queries.py
@@ -0,0 +1,35 @@
+TOP3_ARTICLES = """
+SELECT articles.title, count(*) AS views
+FROM log, articles
+WHERE log.path = '/article/' || articles.slug
+GROUP BY articles.id
+ORDER BY views DESC
+LIMIT 3;
+"""
+
+TOP_AUTHORS = """
+SELECT authors.name, sum(views_by_article) AS views_by_author
+FROM (
+    SELECT articles.author AS author, count(*) AS views_by_article
+    FROM log, articles
+    WHERE log.path = '/article/' || articles.slug
+    GROUP BY articles.id
+    ORDER BY views_by_article
+    ) AS views_by_article_table, authors
+WHERE author = authors.id
+GROUP BY authors.id
+ORDER BY views_by_author DESC;
+"""
+
+ERROR_PERCENTAGE = """
+SELECT *
+FROM (
+    SELECT edb.day, (edb.errors * 100)::decimal / rbd.requests AS percentage
+    FROM errors_by_day edb, requests_by_day rbd
+    WHERE edb.day = rbd.day
+    ) AS percentage_table
+WHERE percentage > 1.0;
+"""
+
+# create view errors_by_day as select time::date as day, count(*) as errors from log where status = '404 NOT FOUND' group by day;
+# create view requests_by_day as select time::date as day, count(*) as requests from log group by day;
